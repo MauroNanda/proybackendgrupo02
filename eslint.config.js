86cf376111c3b28538e89199827ae365ef7a8927
Gutierrez Nanda, Mauro Nahuel <39233689@fi.unju.edu.ar>
2026-06-27 01:07:00 -0300

    chore: inicializar configuración del proyecto y dependencias

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+const js = require('@eslint/js');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+      globals: {
+        process: 'readonly',
+        console: 'readonly',
+        require: 'readonly',
+        module: 'readonly',
+        exports: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        Buffer: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+    },
+    ignores: ['node_modules/', 'dist/', 'coverage/'],
+  },
+];
