e3c05f21ea89a6e64a4071ba5766264cae5c5760
unknown <deliacusipuma80@gmail.com>
2026-06-29 09:40:36 -0300

    feat: crear modelo Evento

---
+++ models/evento.model.js
@@ -0,0 +1,49 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const Evento = sequelize.define(
+    'Evento',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+
+      titulo: {
+        type: DataTypes.STRING(200),
+        allowNull: false,
+      },
+
+      descripcion: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+
+      fecha: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+
+      ubicacion: {
+        type: DataTypes.STRING(200),
+        allowNull: false,
+      },
+
+      cupo_maximo: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+
+      estado: {
+        type: DataTypes.ENUM('BORRADOR', 'PUBLICADO', 'CANCELADO'),
+        defaultValue: 'PUBLICADO',
+      },
+    },
+    {
+      tableName: 'Eventos',
+    }
+  );
+
+  return Evento;
+};
