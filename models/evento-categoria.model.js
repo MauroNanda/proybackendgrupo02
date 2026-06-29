014a28034bff440ada8f838e177e2824362f9399
unknown <deliacusipuma80@gmail.com>
2026-06-29 10:06:14 -0300

    feat: definir modelo EventoCategoria

--- models/evento-categoria.model.js
+++ models/evento-categoria.model.js
@@ -1,7 +1,16 @@
-module.exports = (sequelize) => {
+module.exports = (sequelize, DataTypes) => {
   const EventoCategoria = sequelize.define(
     'EventoCategoria',
-    {},
+    {
+      eventoId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      categoriaId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+    },
     {
       tableName: 'EventoCategorias',
       timestamps: false,
